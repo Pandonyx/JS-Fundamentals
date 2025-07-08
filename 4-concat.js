f13b2d898555bcfdbf246d545d928391aceb3179
Pandonyx <Pandonyx@gmail.com>
2025-07-08 09:52:58 +0100

    concat minor undefined fix

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,5 @@
-if (process.argv[2] === undefined) {
-  console.log("No argument");
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log(process.argv[2] + " is " + process.argv[3]);
 } else {
   console.log(process.argv[2] + " is " + process.argv[3]);
 }
